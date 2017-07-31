d8c31a0bf3f27f285b354545640b9424fa74816d
Atila Neves <aalvesne@cisco.com>
2017-07-31 14:55:46 +0200

    Passing UT for getHeaderFileName

--- source/include/runtime.d
+++ source/include/runtime.d
@@ -1,5 +1,30 @@
 module include.runtime;
 
+version(unittest) import unit_threaded;
+
 void run(string[] args) {
 
+}
+
+private string getHeaderFileName(string line) @safe pure {
+    import std.algorithm: startsWith, countUntil;
+    import std.range: dropBack;
+    import std.array: popFront;
+    import std.string: stripLeft;
+
+    line = line.stripLeft;
+    if(!line.startsWith(`#include `)) return "";
+
+    const openingQuote = line.countUntil!(a => a == '"' || a == '<');
+    const closingQuote = line[openingQuote + 1 .. $].countUntil!(a => a == '"' || a == '>') + openingQuote + 1;
+    return line[openingQuote + 1 .. closingQuote];
+}
+
+@("getHeaderFileName")
+@safe pure unittest {
+    getHeaderFileName(`#include "foo.h"`).shouldEqual(`foo.h`);
+    getHeaderFileName(`#include "bar.h"`).shouldEqual(`bar.h`);
+    getHeaderFileName(`#include "foo.h" // comment`).shouldEqual(`foo.h`);
+    getHeaderFileName(`#include <foo.h>`).shouldEqual(`foo.h`);
+    getHeaderFileName(`    #include "foo.h"`).shouldEqual(`foo.h`);
 }
