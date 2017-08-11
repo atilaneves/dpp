de4702ba0831b7832211730a4e84a98ccadfb755
Atila Neves <aalvesne@cisco.com>
2017-08-11 18:56:49 +0200

    translation module documentation

--- source/include/translation.d
+++ source/include/translation.d
@@ -1,3 +1,13 @@
+/**
+   This module expands each header encountered in the original input file.
+   It usually delegates to dstep but not always. Since dstep has a different
+   goal, which is to produce human-readable D files from a header, we can't
+   just call into it.
+
+   The translate function here will handle the cases it knows how to deal with,
+   otherwise it asks dstep to it for us.
+ */
+
 module include.translation;
 
 
