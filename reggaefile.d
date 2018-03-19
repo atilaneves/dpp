3d4b612dbff59212366727d02f1b6cb94319c3c5
Atila Neves <atila.neves@gmail.com>
2018-03-19 15:02:52 +0100

    Build changes

--- reggaefile.d
+++ reggaefile.d
@@ -1,8 +1,16 @@
 import reggae;
+import std.typecons;
 
 enum debugFlags = "-w -g -debug";
 
 mixin build!(
     dubDefaultTarget!(CompilerFlags(debugFlags)),
-    dubTestTarget!(CompilerFlags(debugFlags)),
+    dubTestTarget!(CompilerFlags(debugFlags), LinkerFlags(), CompilationMode.package_),
+    dubTarget!({Target[] t; return t;})
+    (TargetName("utl"),
+     configToDubInfo["unittest"],
+     debugFlags ~ " -version=unitThreadedLight",
+     [],
+     Yes.main,
+     CompilationMode.module_)
 );
