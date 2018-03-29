c797a84ae4972179030642a89512f16494ecaa8d
Atila Neves <atila.neves@gmail.com>
2018-03-29 22:27:52 +0200

    Refactored examples.sh

--- examples.sh
+++ examples.sh
@@ -2,14 +2,7 @@
 
 set -euo pipefail
 
-bin/include examples/nanomsg.d_ bin/nanomsg.d
-dmd -L-lnanomsg -ofbin/nanomsg bin/nanomsg.d
-bin/nanomsg
-
-bin/include examples/curl.d_ bin/curl.d
-dmd -L-lcurl -ofbin/curl bin/curl.d
-bin/curl
-
-bin/include examples/pthread.d_ bin/pthread.d
-dmd -L-lcurl -ofbin/pthread bin/pthread.d
-bin/pthread
+./example.sh nanomsg
+./example.sh curl
+./example.sh pthread
+./example.sh stdlib
