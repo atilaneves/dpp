5f83f5b3c413865781420c811a163c560ccbdd58
Atila Neves <atila.neves@gmail.com>
2018-04-06 00:29:06 +0200

    Fix at.sh

--- at.sh
+++ at.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
+# Run all acceptance tests
+
 set -euo pipefail
 
-./examples.sh
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BIN_DIR="$SCRIPT_DIR"/bin
+
+# just to shut it up
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+# just to shut it up
+popd () {
+    command popd > /dev/null
+}
+
+# Build an up-to-date binary
+if [[ -e "$BIN_DIR"/build.ninja ]]; then
+    ninja -C "$BIN_DIR" d++ > /dev/null
+else
+    pushd "$SCRIPT_DIR/.."
+    dub build
+    popd
+fi
+
 bundle exec cucumber --tags ~@wip
+./examples/all.sh
--- examples/all.sh
+++ examples/all.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+# Check all the examples work
+
 set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BIN_DIR="$SCRIPT_DIR"/../bin
 
+# just to shut it up
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+# just to shut it up
+popd () {
+    command popd > /dev/null
+}
+
+# Build an up-to-date binary
 if [[ -e "$BIN_DIR"/build.ninja ]]; then
     ninja -C "$BIN_DIR" d++ > /dev/null
 else
