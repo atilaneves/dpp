556d724c82e6285ea3e3dfe0827113f9e84a759e
Atila Neves <atila.neves@gmail.com>
2018-04-05 19:37:49 +0200

    Fix cucumber run

--- ci.sh
+++ ci.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 dub test --build=unittest-cov --compiler="$DC" -- ~@travis
 dub build
-bundle exec cucumber --tags ~@wip ~@notravis
+bundle exec cucumber --tags ~@wip --tags ~@notravis
