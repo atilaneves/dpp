15a9a90877e1b957e8cfae2704e6f8132869a5d0
Atila Neves <atila.neves@gmail.com>
2018-11-22 11:51:44 +0100

    Refactoring

--- source/dpp/translation/macro_.d
+++ source/dpp/translation/macro_.d
@@ -8,14 +8,9 @@ string[] translateMacro(in from!"clang".Cursor cursor,
 {
     import dpp.translation.dlang: maybeRename;
     import clang: Cursor;
-    import std.algorithm: map;
-    import std.string: join;
     import std.file: exists;
-    import std.stdio: File;
     import std.algorithm: startsWith;
-    import std.conv: text, to;
-    import std.exception: collectException;
-    import std.string: strip;
+    import std.conv: text;
 
     assert(cursor.kind == Cursor.Kind.MacroDefinition);
 
@@ -29,15 +24,6 @@ string[] translateMacro(in from!"clang".Cursor cursor,
         return [];
     }
 
-    // now we read the header where the macro comes from and copy the text inline
-
-    const startPos = range.start.offset;
-    const endPos   = range.end.offset;
-
-    auto file = File(range.path);
-    file.seek(startPos);
-    const chars = () @trusted { return file.rawRead(new char[endPos - startPos]); }();
-
     // the only sane way for us to be able to see a macro definition
     // for a macro that has already been defined is if an #undef happened
     // in the meanwhile. Unfortunately, libclang has no way of passing
@@ -48,8 +34,36 @@ string[] translateMacro(in from!"clang".Cursor cursor,
 
     context.rememberMacro(cursor);
     const spelling = maybeRename(cursor, context);
-    const body_ = chars.text[cursor.spelling.length .. $];
+    const body_ = range.chars.text[cursor.spelling.length .. $];
     const dbody = translateToD(body_, context);
+
+    auto redefinition = [maybeUndef ~ "#define " ~ spelling ~ dbody ~ "\n"];
+
+    // Always redefine the macro, but sometimes also add a D enum
+    // See #103 for why.
+    return onlyRedefine(cursor, dbody)
+        ? redefinition
+        : redefinition ~ [`enum DPP_ENUM_` ~ spelling ~ ` = ` ~ dbody ~ `;`];
+}
+
+
+private auto chars(in from!"clang".SourceRange range) @safe {
+    import std.stdio: File;
+
+    const startPos = range.start.offset;
+    const endPos   = range.end.offset;
+
+    auto file = File(range.path);
+    file.seek(startPos);
+    return () @trusted { return file.rawRead(new char[endPos - startPos]); }();
+}
+
+
+// whether the macro should only be re-#defined
+private bool onlyRedefine(in from!"clang".Cursor cursor, in string dbody) @safe pure {
+    import std.string: strip;
+    import std.conv: to;
+    import std.exception: collectException;
 
     const isBodyString = dbody.strip.length >=2 && dbody[0] == '"' && dbody.strip[$-1] == '"';
     long dummyLong;
@@ -58,17 +72,10 @@ string[] translateMacro(in from!"clang".Cursor cursor,
     const isBodyFloating = dbody.strip.to!double.collectException(dummyDouble) is null;
 
     // See #103 for check to where it's a macro function or not
-    const redefineOnly =
+    return
         cursor.isMacroFunction || (!isBodyString && !isBodyInteger && !isBodyFloating);
 
-    auto redefinition = [maybeUndef ~ "#define " ~ spelling ~ dbody ~ "\n"];
-
-    // Always redefine the macro, but sometimes also add a D enum
-    return redefineOnly
-        ? redefinition
-        : redefinition ~ [`enum DPP_ENUM_` ~ spelling ~ ` = ` ~ dbody ~ `;`];
 }
-
 
 // Some macros define snippets of C code that aren't valid D
 private string translateToD(in string line, in from!"dpp.runtime.context".Context context) @safe {
