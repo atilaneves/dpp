ec5ded2342ac240ba34cafbeea6009043beb0314
Atila Neves <atila.neves@gmail.com>
2018-12-22 19:21:48 +0100

    Only do coverage for "old" code

--- ci.sh
+++ ci.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 DC="${DC:-dmd}"
 
 dub test --build=unittest-cov --compiler="$DC"
-dub run -c dpp2 --build=unittest-cov --compiler="$DC"
+dub run -c dpp2 --build=unittest --compiler="$DC"
 dub build --compiler="$DC"
 
 if [[ "$DC" == "dmd" ]]; then
