a9a10eb398dd9fb4d76860d0575f111be0c31910
Laeeth Isharc <laeeth@kaleidic.io>
2018-12-31 06:16:48 +0000

    vec int

--- examples/stl/vec/v.hpp
+++ examples/stl/vec/v.hpp
@@ -12,6 +12,11 @@ using std::pair;
 // typedef map<std::string,std::string> MapStringString;
 // typedef pair<std::string,std::string> PairStringString;
 
+struct VectorInt
+{
+	vector<int> values;
+};
+
 struct Problem
 {
 	vector<double> values;
@@ -56,15 +61,28 @@ void createVector(void* ptr, void* data, size_t size)
 	void** bytesPtr = reinterpret_cast<void**>(ptr);
 	vector<T>* v = reinterpret_cast<vector<T>*>(bytesPtr);
 	T* refData = reinterpret_cast<T*>(data);
-	*v = vector<double>(0);
+	*v = vector<T>(0);
 	for(long i =0; i<size;i++)
 	{
 		v->push_back(refData[i]);
 	}
 }
+
+void* ret[4];
+template<class T>
+void* dummy_(void)
+{
+	ret[0] = (void*) &getVectorSize<T>;
+	ret[1] = (void*) &getVectorData<T>;
+	ret[2]= (void*) &createVector<T>;
+	ret[3] = (void*) &printVector<T>;
+	return &ret;
+}
 double* getVectorDataDouble(void* bytes);
 size_t getVectorFoo(struct Problem* problem);
+void *p_ = dummy_<int>();
 void* p1 = (void*) &getVectorSize<double>;
 void* p2 = (void*) &getVectorData<double>;
 void* p3 = (void*) &createVector<double>;
 void* p4 = (void*) &printVector<double>;
+void *p5 = (void*)&createVector<int>;
