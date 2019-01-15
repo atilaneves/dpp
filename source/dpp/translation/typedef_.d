9539ea043f02ad0376e6f9d88cc769c5e958c966
Atila Neves <atila.neves@gmail.com>
2019-01-15 20:18:35 +0100

    More typedef refactoring

--- source/dpp/translation/typedef_.d
+++ source/dpp/translation/typedef_.d
@@ -9,19 +9,15 @@ string[] translateTypedef(in from!"clang".Cursor typedef_,
                           ref from!"dpp.runtime.context".Context context)
     @safe
 {
-    import dpp.translation.type: translate, isTypeParameter;
-    import dpp.translation.aggregate: isAggregateC;
-    import dpp.translation.dlang: maybeRename;
-    import clang: Cursor, Type;
-    import std.conv: text;
-    import std.typecons: No;
-    import std.algorithm: filter, canFind;
+    import clang: Cursor;
+    import std.algorithm: filter;
     import std.array: array;
 
     const children = () @trusted {
         return typedef_
         .children
         .filter!(a => !a.isInvalid)
+        // ???
         .filter!(a => a.kind != Cursor.Kind.FirstAttr)
         .array;
     }();
@@ -33,34 +29,48 @@ string[] translateTypedef(in from!"clang".Cursor typedef_,
     if(isSomeFunction(typedef_.underlyingType.canonical))
         return translateFunctionTypeDef(typedef_, context.indent);
 
-    const isOnlyAggregateChild = children.length == 1 && isAggregateC(children[0]);
     const isTopLevelAnonymous =
-        isOnlyAggregateChild &&
+        children.length == 1 && // so we can inspect it
         children[0].spelling == "" && // anonymous
         children[0].lexicalParent.kind == Cursor.Kind.TranslationUnit; // top-level
 
     // if the child is a top-level anonymous struct, it's pointless to alias
     // it and give the struct a silly name, instead just define a struct with
     // the typedef name instead. e.g.
-    // typedef struct { int dummy; } Foo -> struct Foo { int dummy; }
+    // `typedef struct { int dummy; } Foo` -> `struct Foo { int dummy; }`
     // However, this isn't true for enums since an anonymous enum can be declared
     // with no typedef. See #54.
     if(isTopLevelAnonymous && children[0].kind != Cursor.Kind.EnumDecl)
         return translateTopLevelAnonymous(children[0], context);
 
+    return translateRegularTypedef(typedef_, context, children);
+}
+
+
+private string[] translateRegularTypedef(in from!"clang".Cursor typedef_,
+                                         ref from!"dpp.runtime.context".Context context,
+                                         in from!"clang".Cursor[] children)
+    @safe
+{
+    import dpp.translation.type: translate;
+    import dpp.translation.aggregate: isAggregateC;
+    import dpp.translation.dlang: maybeRename;
+    import std.typecons: No;
+
     const underlyingSpelling = () {
         switch(typedef_.spelling) {
-            default:
-                // FIXME - still not sure I understand isOnlyAggregateChild here
-                return isOnlyAggregateChild
-                    ? context.spellingOrNickname(children[0])
-                    : translate(typedef_.underlyingType, context, No.translatingFunction);
+        default:
+            // FIXME - still not sure I understand isOnlyAggregateChild here
+            const isOnlyAggregateChild = children.length == 1 && isAggregateC(children[0]);
+            return isOnlyAggregateChild
+                ? context.spellingOrNickname(children[0])
+                : translate(typedef_.underlyingType, context, No.translatingFunction);
 
-            // possible issues on 32-bit
-            case "int32_t":  return "int";
-            case "uint32_t": return "uint";
-            case "in64_t":   return "long";
-            case "uint64_t": return "ulong";
+        // possible issues on 32-bit
+        case "int32_t":  return "int";
+        case "uint32_t": return "uint";
+        case "in64_t":   return "long";
+        case "uint64_t": return "ulong";
         }
     }();
 
