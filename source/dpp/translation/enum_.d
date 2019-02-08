d8b27b0a8711e013c732e480edcdc126ae8a6f8a
Atila Neves <atila.neves@gmail.com>
2019-02-08 17:03:58 +0100

    Refactored enum_

--- source/dpp/translation/enum_.d
+++ source/dpp/translation/enum_.d
@@ -10,13 +10,14 @@ import dpp.from;
 string[] translateEnumConstant(in from!"dpp.ast.node".Node node,
                                ref from!"dpp.runtime.context".Context context)
     @safe
+    in(node.kind == from!"clang".Cursor.Kind.EnumConstantDecl)
+    do
 {
     import dpp.translation.dlang: maybeRename;
-    import clang: Cursor;
     import std.conv: text;
 
-    assert(node.kind == Cursor.Kind.EnumConstantDecl);
     context.log("    Enum Constant Value: ", node.enumConstantValue);
     context.log("    tokens: ", node.tokens);
-    return [maybeRename(node, context) ~ ` = ` ~ text(node.enumConstantValue) ~ `, `];
+
+    return [text(maybeRename(node, context), ` = `, node.enumConstantValue, `, `)];
 }
