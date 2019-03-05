0930806947640a26b11e5f8ab9a438ff4b0454f4
Atila Neves <atila.neves@gmail.com>
2019-03-05 17:35:58 +0100

    Disable dpp2 tests

--- ci.sh
+++ ci.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 DC="${DC:-dmd}"
 
 dub test -q --build=unittest-cov --compiler="$DC"
-dub run -q -c dpp2 --build=unittest --compiler="$DC"
+# dub run -q -c dpp2 --build=unittest --compiler="$DC"
 dub build -q --compiler="$DC"
 
 if [[ "$DC" == "dmd" ]]; then
