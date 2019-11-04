042f1e9c3c99f593a2fdd39e7f3b6887aa4c8051
Atila Neves <atila.neves@gmail.com>
2019-11-04 15:57:12 +0100

    Failing test for typeof language extension

---
+++ tests/it/c/compile/extensions.d
@@ -0,0 +1,29 @@
+/**
+   Non-standard language extensions
+ */
+module it.c.compile.extensions;
+
+
+import it;
+
+
+@ShouldFail
+@("typeof")
+@safe unittest {
+    shouldCompile(
+        C(
+            q{
+                struct Foo;
+                // typeof is a gcc and clang language extension
+                typeof(struct Foo *) func();
+            }
+        ),
+
+        D(
+            q{
+                import std.traits: ReturnType;
+                static assert(is(ReturnType!func == Foo*));
+            }
+        ),
+    );
+}
