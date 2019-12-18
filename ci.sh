7ade046f89242af2396d479d55254006684642a3
Atila Neves <atila.neves@gmail.com>
2019-12-18 11:41:07 +0100

    Explicitly use dub registry IP address

--- ci.sh
+++ ci.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 DC="${DC:-dmd}"
 
-dub test -q --build=unittest-cov --compiler="$DC"
+dub test -q --build=unittest-cov --compiler="$DC" --registry=http://31.15.67.41
 # dub run -q -c dpp2 --build=unittest --compiler="$DC"
-dub build -q --compiler="$DC"
+dub build -q --compiler="$DC" --registry=http://31.15.67.41
 
 if [[ "$DC" == "dmd" ]]; then
     bundle exec cucumber --tags ~@wip
