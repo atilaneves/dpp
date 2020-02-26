a9419418729961a5a7cd94ff3796a834c45a5388
Atila Neves <atila.neves@gmail.com>
2020-02-26 14:15:35 +0100

    Fix #246 - --source-output-path option

--- tests/it/c/compile/runtime_args.d
+++ tests/it/c/compile/runtime_args.d
@@ -57,6 +57,31 @@ import it;
 }
 
 
+@("src output path")
+@safe unittest {
+    with(immutable IncludeSandbox()) {
+        writeFile("hdr.h",
+                  q{
+                      int add(int i, int j);
+                  });
+        writeFile("main.dpp",
+                  `
+                      #include "hdr.h"
+                      void main() {
+                          int ret = add(2, 3);
+                      }
+                  `);
+        runPreprocessOnly(
+            "--source-output-path",
+            inSandboxPath("foo/bar"),
+            "main.dpp",
+        );
+
+        shouldCompile(inSandboxPath("foo/bar/main.d"));
+    }
+}
+
+
 @("rewritten module name")
 @safe unittest {
     with(immutable IncludeSandbox()) {
