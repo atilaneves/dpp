bd45b8d71faf2f823686f40139eee595ba7700c7
Atila Neves <atila.neves@gmail.com>
2021-01-21 18:32:51 +0100

    Remove explicit registry from dub invocation

--- ci.sh
+++ ci.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 DC="${DC:-dmd}"
 
-dub test -q --build=unittest-cov --compiler="$DC" --registry=http://31.15.67.41
+dub test -q --build=unittest-cov --compiler="$DC"
 # dub run -q -c dpp2 --build=unittest --compiler="$DC"
-dub build -q --compiler="$DC" --registry=http://31.15.67.41
+dub build -q --compiler="$DC"
 
 if [[ "$DC" == "dmd" ]]; then
     bundle exec cucumber --tags ~@wip
