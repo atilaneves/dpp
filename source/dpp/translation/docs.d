2b88d06bc7522647af8aba148b74048005ae15cc
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-06-08 15:21:58 -0500

    Remove unused code in getComment

--- source/dpp/translation/docs.d
+++ source/dpp/translation/docs.d
@@ -3,15 +3,12 @@ module dpp.translation.docs;
 import dpp.from;
 
 /** Get the attached comments at the location of the cursor */
-string getComment(in from!"clang".Cursor cursor, bool breakln = false) @safe {
+string getComment(in from!"clang".Cursor cursor) @safe {
     import std.typecons: Nullable;
     string docStr = "";
     Nullable!string nullableComment = cursor.raw_comment();
     if(!nullableComment.isNull) {
         docStr = nullableComment.get();
-        if (breakln) {
-            docStr ~= "\n    ";
-        }
     }
     return docStr;
 }
